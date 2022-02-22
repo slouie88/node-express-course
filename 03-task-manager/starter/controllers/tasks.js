14867a9c3c8ed60c07bf1966c2df0cc3134b4900
stefan <stefan.louie@gmail.com>
2022-02-22 22:39:02 +1000

    Modified all controllers except updateTask to perform CRUD operations with the mongoDB Tasks table.

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,23 +1,60 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-    res.send('all tasks')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks: tasks })
+    }
+    catch(err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    }
+    catch(err) {
+        res.status(500).json({ msg: err})
+    }
 }
 
-const getTask = (req, res) => {
-    res.json( { id: req.params.id })
+const getTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOne({ _id: taskID })
+
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` })
+        }
+
+        res.status(200).json({ task })
+    }
+    catch(err) {
+        res.status(500).json({ msg: err })
+    }
+    
 }
 
 const updateTask = (req, res) => {
     res.send('edit task')
 }
 
-const deleteTask = (req, res) => { 
-    res.send('delete task')
+const deleteTask = async (req, res) => { 
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({ _id: taskID })
+
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` })
+        }
+
+        res.status(200).json({ task })
+    }
+    catch(err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
 module.exports = {
