8b92419d4da197ffbd126ceb5b1468a0ae7dce14
stefan <stefan.louie@gmail.com>
2022-02-23 21:27:41 +1000

    completed basic controllers for crud operations.

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -37,8 +37,23 @@ const getTask = async (req, res) => {
     
 }
 
-const updateTask = (req, res) => {
-    res.send('edit task')
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` })
+        }
+
+        res.status(200).json({ task })
+    }
+    catch(err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
 const deleteTask = async (req, res) => { 
