35e980818c4b288133e54dd09105320736128542
stefan <stefan.louie@gmail.com>
2022-02-27 17:24:12 +1000

    Set up field selection and pagination for store products.

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,17 @@
-const { query } = require('express')
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
     const search = 'a'
-    const products = await Product.find({ 
-        name: { $regex: search, $options: 'i' }
-    })
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10)
+    .skip(5)
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -22,9 +23,29 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' }
     }
-    console.log(queryObject)
 
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+
+    // Sort
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+    // Fields
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+    // Pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
+
+    const products = await result
     res.status(200).json ({ products, nbHits: products.length })
 }
 
