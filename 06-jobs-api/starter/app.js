761566597a27faad7c2e8b4b62197a6a3ecaa08f
stefan <stefan.louie@gmail.com>
2022-03-11 15:34:15 +1000

    Added some extra security packages in jobs-api project.

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// Extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -15,8 +22,17 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+app.set('trust proxy', 1)
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+})
+)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
-// extra packages
 
 // routes
 app.use('/api/v1/auth', authRouter)
